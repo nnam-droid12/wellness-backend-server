8952d1039083e5e05e57c441a983cc6edd8a160d
willitech <williamnnamani@yahoo.com>
2024-06-15 14:54:46 +0100

    setup mongodb database

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = require('express');
+const http = require('http');
+
+const app = require('./app');
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
 
 
-
-const app = express();
 const PORT = process.env.PORT || 5000
+const mongoURL = process.env.MONGO_URL
+
+const server = http.createServer(app);
+
+mongoose.connect(mongoURL)
+.then(() => console.log('db connected'))
+.catch((err) => console.log(err));
 
 
-app.use(express.json());
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
-

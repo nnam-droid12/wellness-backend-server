ac86cb1a5a785bf7e45b41772ac27f2b99f8429c
willitech <williamnnamani@yahoo.com>
2024-06-15 06:24:22 +0100

    setup backend server for pipops

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
+

92c451b4211fec58e4e48431549743f0841ccbed
willitech <williamnnamani@yahoo.com>
2024-06-18 20:51:56 +0100

    added cors for cross origin

--- server.js
+++ server.js
@@ -11,16 +11,23 @@ const Hospital = require("./model/model.hospital");
 const User = require("./model/model.user");
 const Review = require("./model/model.review");
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 
 dotenv.config()
 
 const port = process.env.PORT || 5000;
+const corsOptions = {
+     origin: "http://localhost:3000",
+     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+     credentials: true,
+};
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors(corsOptions));
 
 
 app.use("/api/user", user);
